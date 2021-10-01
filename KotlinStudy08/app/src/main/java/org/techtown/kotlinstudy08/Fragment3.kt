022f4344cfdb8e7e04001fb8c15a86deed2a0c19
aerim-choi <dofla5518@naver.com>
2021-10-01 19:23:50 +0900

    2021_10_01_404p코틀린과제08

---
+++ KotlinStudy08/app/src/main/java/org/techtown/kotlinstudy08/Fragment3.kt
@@ -0,0 +1,18 @@
+package org.techtown.kotlinstudy08
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import org.techtown.kotlinstudy08.R
+
+class Fragment3 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_3, container, false)
+    }
+
+}
